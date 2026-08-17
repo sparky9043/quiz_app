195a6a4ac1fd4d4ec33ffff04e56731a0881fbe6
Se Joon Park <sparky9043@gmail.com>
2026-08-17 12:02:58 +0900

    create /api/users route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import usersRouter from './routes/users.ts';
 
 const app = express();
 
+app.use(express.json());
 
+app.use('/api/users', usersRouter);
 
 export default app;
