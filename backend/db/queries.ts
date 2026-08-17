7b6dacac9a32ab0d70b80eaad18d0973f1c5e5c1
Se Joon Park <sparky9043@gmail.com>
2026-08-17 13:34:41 +0900

    add comments to query functions

--- backend/db/queries.ts
+++ backend/db/queries.ts
@@ -1,5 +1,6 @@
 import pool from "./pool.ts";
 
+// Returns Users list with password hash
 const getUsers = async () => {
   const { rows } = await pool.query(`
     SELECT * FROM users;
@@ -8,13 +9,14 @@ const getUsers = async () => {
   return rows;
 };
 
+// Returns Users list with no password hash
 const getUsersNoPassword = async () => {
   const { rows } = await pool.query(`
     SELECT id, username, type FROM users;
   `);
 
   return rows;
-}
+};
 
 export default {
   getUsers,
