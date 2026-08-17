f4c125d87628bc7d7b68f2c189b582983f1bf8c4
Se Joon Park <sparky9043@gmail.com>
2026-08-17 11:36:29 +0900

    import server port from config.ts using dotenv

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import app from "./app.ts";
+import config from "./utils/config.ts";
 
 
-const PORT = 3000;
+const PORT = config.SERVER_PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
