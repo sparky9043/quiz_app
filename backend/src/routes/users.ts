d4c15a3e74d1fe9fcacad30033de2f0d1ce88528
Se Joon Park <sparky9043@gmail.com>
2026-08-17 13:43:38 +0900

    create users routes and service getUserById functions

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,12 +1,27 @@
 import { Router } from "express";
+import type { Request } from "express";
 import userService from "../service/users.ts";
 
 const users = Router();
 
+// User route for /api/users
 users.get('/', async (_req, res) => {
   const users = await userService.getUsersNoPassword();
 
   res.json(users);
+});
+
+// User route for /api/users/id
+users.get('/:id', async (req: Request<{ id: string}>, res) => {
+  const userId = Number(req.params.id);
+
+  if (isNaN(userId)) {
+    throw new Error('invalid Id');
+  }
+
+  const savedUser = await userService.getUserById(userId);
+
+  res.json(savedUser);
 });
 
 export default users;
--- backend/src/service/users.ts
+++ backend/src/service/users.ts
@@ -1,11 +1,21 @@
 import queries from "../../db/queries.ts";
+import type { User } from "../types/user.ts";
 
+// Get users list with no password
 const getUsersNoPassword = async () => {
   const users = await queries.getUsersNoPassword();
 
   return users;
 };
 
+// Get one user with ID
+const getUserById = async (userId: number): Promise<User> => {
+  const user = await queries.getUserById(userId);
+  
+  return user;
+};
+
 export default {
   getUsersNoPassword,
+  getUserById,
 };
