2110d01d450aef9df7067db243ff0967757a47ca
Se Joon Park <sparky9043@gmail.com>
2026-08-17 13:33:19 +0900

    create other user types extending from BaseUser

--- backend/src/types/user.ts
+++ backend/src/types/user.ts
@@ -1,5 +1,16 @@
-export interface UserNoPassword {
-  id: number;
+export interface BaseUser {
   username: string;
   type: 'teacher' | 'student';
 }
+
+export interface UserNoPassword extends BaseUser {
+  id: number;
+}
+
+export interface User extends UserNoPassword {
+  password_hash: string;
+}
+
+export interface NewUser extends BaseUser {
+  password: string;
+};
