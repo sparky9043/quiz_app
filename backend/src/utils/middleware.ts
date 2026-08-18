c2673d015ab602def9bddec49c229a72638ecdcc
Se Joon Park <sparky9043@gmail.com>
2026-08-18 15:03:15 +0900

    return status 409 for database error and 500 for server error

--- backend/src/utils/middleware.ts
+++ backend/src/utils/middleware.ts
@@ -1,7 +1,14 @@
 import type { NextFunction, Request, Response } from "express";
+import { DatabaseError } from "pg";
 // import { DatabaseError } from "pg";
 
 const errorHandler = (err: unknown, _req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof DatabaseError) {
+    if (err.code == '23505') {
+      res.status(409).json({ error: err.message });
+    }
+  }
+
   res.status(500);
   res.render('error', { error: err });
 };
