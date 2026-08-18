4de122922fb76827de3f7a92e2c3b78d1aea9bae
Se Joon Park <sparky9043@gmail.com>
2026-08-18 22:10:58 +0900

    create quiz router and get path for /api/quizzes

--- backend/src/routes/quizRouter.ts
+++ backend/src/routes/quizRouter.ts
@@ -1,5 +1,16 @@
+import type { NextFunction, Request, Response } from "express";
 import { Router } from "express";
+import middleware from "../utils/middleware.ts";
 
 const quizRouter = Router();
+
+quizRouter.get('/', middleware.tokenExtractor, async (req: Request, _res: Response, next: NextFunction) => {
+  try {
+    const token = req.get('authorization');
+    console.log(token);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default quizRouter;
