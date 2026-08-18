01f22d2ab02fdabe6336f4504809cd7dd5498ac5
Se Joon Park <sparky9043@gmail.com>
2026-08-18 17:24:46 +0900

    create login router for handling requests to /api/login

--- backend/src/routes/login.ts
+++ backend/src/routes/login.ts
@@ -1,12 +1,21 @@
 import type { NextFunction, Request, Response } from "express";
 import { Router } from "express";
+import type { UserLoginCredentials } from "../types/user.ts";
+import loginService from "../service/loginService.ts";
 
 const loginRouter = Router();
 
-loginRouter.post('/', async (req: Request, res: Response, next: NextFunction) => {
+loginRouter.post('/', async (req: Request<unknown, unknown, UserLoginCredentials>, res: Response, next: NextFunction) => {
   try {
-    console.log(req, res);
-    res.status(201).json({ status: 'success' });
+    const userLoginCredentals = req.body;
+
+    if (!userLoginCredentals.username || !userLoginCredentals.password) {
+      throw new Error('please enter both the username and password');
+    }
+
+    const response = await loginService.login(userLoginCredentals);
+
+    res.status(201).json(response);
   } catch (error) {
     next(error);
   }
