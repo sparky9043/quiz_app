473ec9998440eeb57891ef89e3ec0d524064293b
Se Joon Park <sparky9043@gmail.com>
2026-08-18 21:54:06 +0900

    create quiz router to path /api/quizzes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import userRouter from './routes/userRouter.ts';
 import middleware from './utils/middleware.ts';
 import loginRouter from './routes/login.ts';
+import quizRouter from './routes/quizRouter.ts';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 // Routers
 app.use('/api/login', loginRouter);
 app.use('/api/users', userRouter);
+app.use('/api/quizzes', quizRouter);
 
 // Error Handler Middlewares
 app.use(middleware.databaseErrorHandler);
---
+++ backend/src/routes/quizRouter.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const quizRouter = Router();
+
+export default quizRouter;
