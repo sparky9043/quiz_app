db3de8574a8ffa2535b4f121a83f4549e0b1025a
Se Joon Park <sparky9043@gmail.com>
2026-08-18 14:24:46 +0900

    refactor helper functions to create/delete db and add users

---
+++ backend/src/tests/helper.ts
@@ -0,0 +1,40 @@
+import pool from "../../db/pool.ts";
+import type { NewUser } from "../types/user.ts";
+import pwd from "../utils/pwd.ts";
+
+const deleteUserTable = async () => {
+  await pool.query(`
+    DROP TABLE users;
+  `);
+};
+
+const createUserTable = async () => {
+  await pool.query(`
+    CREATE TABLE users (
+      id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      username VARCHAR( 255 ) UNIQUE,
+      password_hash VARCHAR( 255 ),
+      type TEXT
+    )
+  `);
+};
+
+const addUserToTable = async (newUser: NewUser) => {
+  const passwordHash = await pwd.hash(newUser.password);
+
+  await pool.query(`
+    INSERT INTO users (username, password_hash, type)
+    VALUES ($1, $2, $3);
+  `, [
+    newUser.username,
+    passwordHash,
+    newUser.type,
+  ]);
+
+};
+
+export default {
+  deleteUserTable,
+  createUserTable,
+  addUserToTable,
+}
