8f65dc2046b73f038cf55f98cf5af7f9c1208af3
Se Joon Park <sparky9043@gmail.com>
2026-08-18 22:14:38 +0900

    add try/catch in all /api/users paths

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,32 +1,38 @@
 import { Router } from "express";
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import userService from "../service/userService.ts";
-import type { NewUser, User } from "../types/user.ts";
+import type { NewUser, User, UserNoPassword } from "../types/user.ts";
 
 const userRouter = Router();
 
 // GET User route for /api/users
-userRouter.get('/', async (_req, res) => {
-  const users = await userService.getUsersNoPassword();
-
-  res.json(users);
+userRouter.get('/', async (_req: Request, res: Response<UserNoPassword[]>, next: NextFunction) => {
+  try {
+    const users = await userService.getUsersNoPassword();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // GET User route for /api/users/id
-userRouter.get('/:id', async (req: Request<{ id: string}>, res) => {
+userRouter.get('/:id', async (req: Request<{ id: string }>, res: Response<UserNoPassword>, next: NextFunction) => {
   const userId = Number(req.params.id);
 
   if (isNaN(userId)) {
     throw new Error('invalid Id');
   }
 
-  const savedUser = await userService.getUserByIdNoPassword(userId);
-
-  res.json(savedUser);
+  try {
+    const savedUser = await userService.getUserByIdNoPassword(userId);
+    res.json(savedUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // POST request to /api/users creates new user
-userRouter.post('/', async (req: Request<unknown, unknown, NewUser>, res: Response<User>) => {
+userRouter.post('/', async (req: Request<unknown, unknown, NewUser>, res: Response<User>, next: NextFunction) => {
   const newUserRequestBody = req.body;
   const userTypes = ['student', 'teacher'];
 
@@ -41,9 +47,13 @@ userRouter.post('/', async (req: Request<unknown, unknown, NewUser>, res: Respon
     throw new Error('Please make sure the username, password and type are correctly filled out');
   }
 
-  const newUser = await userService.createNewUser(newUserRequestBody);
-
-  res.status(201).json(newUser);
+  try {
+    const newUser = await userService.createNewUser(newUserRequestBody);
+  
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default userRouter;
