6ae7fb0e1176c9e645480dd32487ee762b73c72c
Se Joon Park <sparky9043@gmail.com>
2026-08-18 17:00:37 +0900

    make catch-all errorHandler display proper error message

--- backend/src/utils/middleware.ts
+++ backend/src/utils/middleware.ts
@@ -31,8 +31,9 @@ const databaseErrorHandler = (err: unknown, _req: Request, res: Response, next: 
 
 const errorHandler = (err: unknown, _req: Request, res: Response, _next: NextFunction) => {
   console.log('code reached');
-  res.status(500);
-  res.render('error', { error: err });
+  if (err instanceof Error) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 export default {
