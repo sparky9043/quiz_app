edad81b603faba04adc53dfa24ca4b4862fa2a69
Se Joon Park <sparky9043@gmail.com>
2026-08-19 10:33:17 +0900

    ensure supertest sends auth and receives token back

--- backend/src/tests/quiz_api.test.ts
+++ backend/src/tests/quiz_api.test.ts
@@ -5,8 +5,10 @@ import app from '../app.ts';
 import supertest from 'supertest';
 import helper from './helper.ts';
 import type { NewUser } from '../types/user.ts';
+import type { LoginSuccessObject } from '../types/login.ts';
 
 const loginUrl = '/api/login';
+const quizUrl = '/api/quizzes';
 
 const agent = supertest.agent(app);
 
@@ -30,25 +32,29 @@ beforeEach(async () => {
 });
 
 void describe('After Logging in', async () => {
-
   const response = await agent
     .post(loginUrl)
     .send(helper.defaultUserCredentials)
     .expect(200);
+  
+  const successObject = response.body as LoginSuccessObject;
+
+  const tokenBearer = "Bearer " + successObject.token;
 
   void test('true equals true', async () => {
-
-
-    console.log(response.body);
-
-    assert.strictEqual(true, true);
-  });
-
-  void test('test to see if login persists', async () => {
-    console.log(response.body);
+    await agent
+      .get(quizUrl)
+      .set('Authorization', tokenBearer)
+      .expect(200);
 
     assert.strictEqual(true, true);
   });
+
+  // void test('test to see if login persists', () => {
+  //   console.log(response.body);
+
+  //   assert.strictEqual(true, true);
+  // });
 });
 
 after(async () => {
