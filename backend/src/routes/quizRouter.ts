2d95497866d2c9a272889cb98ca340b873a8fd06
Se Joon Park <sparky9043@gmail.com>
2026-08-19 10:33:47 +0900

    ensure token received in the router

--- backend/src/routes/quizRouter.ts
+++ backend/src/routes/quizRouter.ts
@@ -1,14 +1,25 @@
 import type { NextFunction, Request, Response } from "express";
 import { Router } from "express";
 import middleware from "../utils/middleware.ts";
+import jwt from "../utils/jwt.ts";
+import config from "../utils/config.ts";
+// import { HttpError } from "../errors/http.ts";
 
 const quizRouter = Router();
 
-quizRouter.get('/', middleware.tokenExtractor, (req: Request, _res: Response, next: NextFunction) => {
+quizRouter.get('/', middleware.tokenExtractor, (req: Request, res: Response, next: NextFunction) => {
   try {
     // Dont forget to add async/await into this block of code
     const token = req.get('authorization');
-    console.log(token);
+
+    if (!token) {
+      throw new Error('no token found');
+    }
+
+    jwt.verifyToken(token, config.SECRET);
+    
+
+    res.status(200).send('something');
   } catch (error) {
     next(error);
   }
