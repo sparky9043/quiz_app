02713ad7b9718baa3e81d35b0499c5b3a5e11aa9
Se Joon Park <sparky9043@gmail.com>
2026-08-31 10:36:14 +0900

    change App spec file to test file and move to src folder

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from '../App';
+import App from './App';
 
 describe('App component', () => {
   it('renders the Vite + React heading', () => {
