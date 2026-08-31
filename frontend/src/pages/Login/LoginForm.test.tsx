e0628720b88d078448a2edef3b081d6258a85086
Se Joon Park <sparky9043@gmail.com>
2026-08-31 12:22:25 +0900

    create basic test for LoginForm and its inputs

---
+++ frontend/src/pages/Login/LoginForm.test.tsx
@@ -0,0 +1,28 @@
+import LoginForm from "./LoginForm";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+
+describe('LoginForm component', () => {
+  it('renders input elements and login button to page', () => {
+    render(<LoginForm />);
+
+    expect(screen.getByLabelText('username')).toBeInTheDocument();
+    expect(screen.getByLabelText('password')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'login' })).toBeInTheDocument();
+  });
+
+  it('allows user to enter value to input elements', async () => {
+    const user = userEvent.setup();
+
+    render(<LoginForm />);
+
+    const usernameInputEl = screen.getByLabelText<HTMLInputElement>('username');
+    const passwordInputEl = screen.getByLabelText<HTMLInputElement>('password');
+
+    await user.type(usernameInputEl, 'ms_rivera');
+    await user.type(passwordInputEl, 'password123');
+
+    expect(usernameInputEl.value).toEqual('ms_rivera');
+    expect(passwordInputEl.value).toEqual('password123');
+  });
+});
