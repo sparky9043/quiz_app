0d994b869bb15c1bee32bdc45dee1adcf8320e00
Se Joon Park <sparky9043@gmail.com>
2026-08-31 11:41:40 +0900

    add react router Link component to Home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router";
+
 const Home = () => {
   return (
     <div>
+      <nav>
+        <Link to="login">login</Link>
+      </nav>
       <h1>Welcome to my quiz website</h1>
     </div>
   )
