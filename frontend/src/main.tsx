5bedf3db14944570cf6de612cb3f819784041e9d
Se Joon Park <sparky9043@gmail.com>
2026-08-31 10:51:34 +0900

    provide routes to RouterProvider on main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './AppRoutes.tsx'
+import { RouterProvider } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={App} />
   </StrictMode>,
 )
