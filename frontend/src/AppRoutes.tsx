58016cabc0baa4b33d0c3f76456f60112addf7c6
Se Joon Park <sparky9043@gmail.com>
2026-08-31 12:06:37 +0900

    render LoginPage on /login path

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./pages/Home/Home";
+import LoginPage from "./pages/Login/LoginPage";
 
 const AppRoutes = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
     index: true,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
   },
 ]);
 
