736f5f7e77b9165f17afd6e669228847b2345fb3
Se Joon Park <sparky9043@gmail.com>
2026-08-31 11:40:10 +0900

    wrap BrowserRouter around Home component to make React Router component work

--- frontend/src/pages/Home.test.tsx
+++ frontend/src/pages/Home.test.tsx
@@ -1,10 +1,36 @@
 import { render, screen } from "@testing-library/react";
 import Home from "./Home";
+import { BrowserRouter } from "react-router";
 
 describe('Home Component', () => {
   it('renders Welcome message', () => {
-    render(<Home />);
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
   
     expect(screen.getByText(/Welcome/i)).toBeInTheDocument();
-  })
+  });
+
+  it('renders link to login', () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText(/login/)).toBeInTheDocument();
+  });
+
+  it('moves to Login page when clicking login link', () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+
+    expect(true).toEqual(true);
+  });
+  
 });
