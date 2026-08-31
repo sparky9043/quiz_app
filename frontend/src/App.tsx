424a8e5226e01727fd52892be2d175df3b69a34f
Se Joon Park <sparky9043@gmail.com>
2026-08-31 10:51:01 +0900

    rename App.tsx to AppRoutes.tsx and use it as Routes

--- frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-const App = () => {
-  return (
-    <h1>Get Started</h1>
-  )
-};
-
-export default App;
---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+import Home from "./pages/Home";
+
+const AppRoutes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    index: true,
+  },
+]);
+
+export default AppRoutes;
