b5005d66efaa16091f4dedf1fe59e011fb6194ab
Se Joon Park <sparky9043@gmail.com>
2026-08-31 12:14:28 +0900

    create basic controlled input user login form

--- frontend/src/pages/Login/LoginForm.tsx
+++ frontend/src/pages/Login/LoginForm.tsx
@@ -1,13 +1,50 @@
+import { useState } from "react";
+
 const LoginForm = () => {
   const handleLogin = (event: React.SubmitEvent<HTMLFormElement>) => {
     event.preventDefault();
+    console.log(username, password);
   }
+
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   return(
     <form onSubmit={handleLogin}>
-      <h2>Login to your account</h2>
+      <ul>
+        <li>
+          <div>
+            <label htmlFor="username">
+              username
+              <input
+                id="username"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+          </div>
+        </li>
+        <li>
+          <div>
+            <label htmlFor="password">
+              password
+              <input
+                id="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+        </li>
+        <li>
+          <div>
+            <button type="submit">login</button>
+          </div>
+        </li>
+      </ul>
 
-      <button type="submit">login</button>
     </form>
   )
 
