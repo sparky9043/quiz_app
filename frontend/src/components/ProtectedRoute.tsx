08869e9969edf8989893ac6f3b5488b07187c9b3
Se Joon Park <sparky9043@gmail.com>
2026-09-01 14:36:46 +0900

    extract token and redirect if token nonexistent

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,13 +1,25 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
+import type { UserLoginSuccessObject } from "../types/user";
 
-// interface ProtectedRouteProps {
-//   children: React.ReactNode
-// }
+let token: string;
+
+const setToken = (newToken: string) => {
+  token = `Bearer ${newToken}`;
+}
 
 const ProtectedRoute = () => {
+  const userLoginString = localStorage.getItem('userLoginSuccess');
+
+  if (!userLoginString) {
+    return <Navigate to='/login' replace />
+  }
+
+  const userLoginJSON = JSON.parse(userLoginString) as UserLoginSuccessObject;
+  setToken(userLoginJSON.token);
+
   return (
     <div>
-      <Outlet />  
+      <Outlet />
     </div>
   )
 };
