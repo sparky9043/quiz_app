8e3426692db4cdaad2912d9a2e6570da3d2be759
Se Joon Park <sparky9043@gmail.com>
2026-09-01 14:48:12 +0900

    remove token extracting logic from ProtectedRoute

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,27 +1,11 @@
 import { Navigate, Outlet } from "react-router";
-import type { UserLoginSuccessObject } from "../types/user";
-
-let token: string;
-
-const setToken = (newToken: string) => {
-  token = `Bearer ${newToken}`;
-}
 
 const ProtectedRoute = () => {
   const userLoginString = localStorage.getItem('userLoginSuccess');
 
-  if (!userLoginString) {
-    return <Navigate to='/login' replace />
-  }
-
-  const userLoginJSON = JSON.parse(userLoginString) as UserLoginSuccessObject;
-  setToken(userLoginJSON.token);
-
-  console.log(token);
-
   return (
     <div>
-      <Outlet />
+      {userLoginString ? <Outlet /> : <Navigate to='/login' replace />}
     </div>
   )
 };
