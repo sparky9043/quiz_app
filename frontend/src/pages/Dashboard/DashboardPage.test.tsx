e89620a6c2820ae67e137e54b78a2e93a19f49e0
Se Joon Park <sparky9043@gmail.com>
2026-09-01 15:01:02 +0900

    add Link to Dashboard nav and wrap BrowserRouter to pass test

--- frontend/src/pages/Dashboard/DashboardPage.test.tsx
+++ frontend/src/pages/Dashboard/DashboardPage.test.tsx
@@ -1,9 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import DashboardPage from "./DashboardPage";
+import { BrowserRouter } from "react-router";
 
 describe('DashboardPage Component', () => {
   it('renders Dashboard header', () => {
-    render(<DashboardPage />);
+    render(<DashboardPage />, { wrapper: BrowserRouter });
 
     expect(screen.getByText(/Dashboard/i)).toBeInTheDocument();
   });
--- frontend/src/pages/Dashboard/DashboardPage.tsx
+++ frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router";
+
 const DashboardPage = () => {
   return (
     <div>
+      <nav>
+        <li>
+          <Link to='quiz'>Quiz</Link>
+        </li>
+      </nav>
       <h1>Dashboard</h1>
     </div>
   )
