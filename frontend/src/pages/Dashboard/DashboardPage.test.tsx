0c03b58b16290bd5d12676257862b15bbe8f3df0
Se Joon Park <sparky9043@gmail.com>
2026-09-01 15:37:05 +0900

    make DashboardPage test pass using MemoryRouter
    I noticed that when I just use MemoryRouter with regular paths
    using initialEntries, this did not properly render the path nor did
    it correctly render the Outlet component as desired. Therefore,
    I decided to render a set of React Router components directly
    in this hierarchy: MemoryRouter > Routes > Route (path='/dashboard')
    > Route (path='quiz')
    This mocked the desired path and also rendered the desired component
    inside the Outlet.

--- frontend/src/pages/Dashboard/DashboardPage.test.tsx
+++ frontend/src/pages/Dashboard/DashboardPage.test.tsx
@@ -1,11 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import DashboardPage from "./DashboardPage";
-import { BrowserRouter } from "react-router";
+import { MemoryRouter, Route, Routes } from "react-router";
+import QuizPage from "../Quiz/QuizPage";
 
 describe('DashboardPage Component', () => {
-  it('renders Dashboard header', () => {
-    render(<DashboardPage />, { wrapper: BrowserRouter });
+  it('renders Dashboard header if on /dashboard', () => {
+    const dashboardRoute = '/dashboard';
+
+    render(
+      <MemoryRouter initialEntries={[dashboardRoute]}>
+        <DashboardPage />
+      </MemoryRouter>
+    );
 
     expect(screen.getByText(/Dashboard/i)).toBeInTheDocument();
+  });
+
+  it('renders Quiz header if on /dashboar/quiz', () => {
+    const quizRoute = '/dashboard/quiz'
+
+    render(
+      <MemoryRouter initialEntries={[quizRoute]}>
+        <Routes>
+          <Route path='/dashboard' element={<DashboardPage />}>
+            <Route path='quiz' element={<QuizPage />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    )
+
+    expect(screen.getByText(/Quiz Page/i)).toBeInTheDocument();
   });
 });
