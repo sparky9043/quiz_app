b0363e8a5de852e1b0650046064904ca3c1e9667
Se Joon Park <sparky9043@gmail.com>
2026-09-01 14:29:39 +0900

    create UserLoginSuccessObject type

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -2,3 +2,10 @@ export interface UserLoginCredentials {
   username: string;
   password: string;
 }
+
+export interface UserLoginSuccessObject {
+  token: string;
+  id: number;
+  type: string;
+  username: string;
+}
