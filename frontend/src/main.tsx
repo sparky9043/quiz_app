6795a2460d718f82d1b341d4d7620043253157a1
Se Joon Park <sparky9043@gmail.com>
2026-09-10 11:08:34 +0900

    wrap whole app in QueryProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,14 @@ import './tailwind-config.css'
 import './reset.css'
 import AppRoutes from './AppRoutes.tsx'
 import { RouterProvider } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={AppRoutes} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={AppRoutes} />
+    </QueryClientProvider>
   </StrictMode>,
 )
