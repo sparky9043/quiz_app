1b7b03015e20bb941ac7d87c6ceaa6e3aaced68d
Se Joon Park <sparky9043@gmail.com>
2026-09-14 14:11:10 +0900

    move timestamp from quizRequestSchema and to quizSchema

--- backend/src/schema/quiz.schema.ts
+++ backend/src/schema/quiz.schema.ts
@@ -2,12 +2,12 @@ import * as z from 'zod';
 
 const quizRequestSchema = z.object({
   teacher_id: z.number(),
-  title: z.string(),
-  timestamps: z.string(),
+  title: z.string()
 });
 
 const quizSchema = quizRequestSchema.extend({
   id: z.number(),
+  timestamps: z.string(),
 });
 
 export { quizRequestSchema, quizSchema };
