9a548f16592acd6898a0fd95d41a47232c9be230
Se Joon Park <sparky9043@gmail.com>
2026-09-14 15:48:24 +0900

    make QuizCreateForm create new quiz and navigate to /dashboard/quiz/:id if successful

--- frontend/src/pages/Quiz/QuizCreateForm.tsx
+++ frontend/src/pages/Quiz/QuizCreateForm.tsx
@@ -1,10 +1,54 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import type { Quiz } from "../../types/quiz";
+
 const QuizCreateForm = () => {
-  const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event: React.SubmitEvent<HTMLFormElement>) => {
     event.preventDefault();
+    try {
+      const loginSuccessObjectJSON = localStorage.getItem('userLoginSuccess');
+      
+      if (loginSuccessObjectJSON) {
+        const loginSuccessObjectParsed = JSON.parse(loginSuccessObjectJSON);
+        
+        const teacherId = loginSuccessObjectParsed.id;
+
+        const quizRequest = {
+          title,
+          teacher_id: Number(teacherId),
+        }
+
+        const response = await axios
+          .post<Quiz>('/api/quizzes', quizRequest, {
+            headers: {
+              "Authorization": `Bearer ${loginSuccessObjectParsed.token}`,
+            },
+          });
+        
+        navigate(`/dashboard/quiz/${response.data.id}`);
+      }
+
+    } catch (error) {
+      throw error;
+    }
+
   };
+
+  const [title, setTitle] = useState<string>('');
 
   return (
     <form onSubmit={handleSubmit}>
+      <label htmlFor="title">
+        <input
+          id="title"
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </label>
       <button type="submit">create quiz</button>
     </form>
   )
